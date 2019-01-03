e204b031129af94f3476fd3f97d88a5621a52f15
Sihong Huang <sihongspring2011@gmail.com>
2019-01-03 17:02:39 -0500

    work on burger state, burger controls

--- s8-burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ s8-burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -13,9 +13,15 @@ const controls = [
 const BuildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
+      <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
       {
         controls.map(control => {
-          return <BuildControl key={control.label} label={control.label} type={control.type} />
+          return <BuildControl 
+                    key={control.label} 
+                    label={control.label} 
+                    added={() => props.ingredientAdded(control.type)}
+                    removed={() => props.ingredientRemoved(control.type)}
+                    disabled={props.disabled[control.type]} />
         })
       }
     </div>
