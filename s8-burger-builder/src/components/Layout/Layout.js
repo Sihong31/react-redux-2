77b730c36cb95071f78b048ae60fcdb4cb6023ff
Sihong Huang <sihongspring2011@gmail.com>
2019-01-03 16:19:08 -0500

    work on burger components

--- s8-burger-builder/src/components/Layout/Layout.js
+++ s8-burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './Layout.css';
+
 // implicit return
 const Layout = (props) => (
   <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className= {classes.Content}>
       { props.children }
     </main>
   </>
