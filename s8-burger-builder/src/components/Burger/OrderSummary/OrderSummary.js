1960e42594af9cd4559b6eabe8eb9b858293c70c
Sihong Huang <sihongspring2011@gmail.com>
2019-01-04 14:15:51 -0500

    move layout component, performance enhancements

--- s8-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ s8-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+  componentWillUpdate() {
+    console.log('order summary will update');
+  }
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(ingredientKey => {
       return (
         <li key={ingredientKey}>
-          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {props.ingredients[ingredientKey]}
+          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
         </li>
       )
     })
 
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>Delicious Burger with the following ingredients:</p>
-      <ul>
-        { ingredientSummary }
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </>
-  );
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>Delicious Burger with the following ingredients:</p>
+        <ul>
+          { ingredientSummary }
+        </ul>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </>
+    );
+  }
 }
 
-export default OrderSummary
+export default OrderSummary;
--- s8-burger-builder/src/components/UI/Modal/Modal.js
+++ s8-burger-builder/src/components/UI/Modal/Modal.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-  return(
-    <>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div 
-        className={classes.Modal} 
-        style={{
-          transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-          {props.children}
-      </div>
-    </>
-  );
+class Modal extends Component {
+
+  // check if content of modal should be updated when order now button is clicked
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log('modal willupdate')
+  }
+
+  render() {
+    return(
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div 
+          className={classes.Modal} 
+          style={{
+            transform: this.props.show ? 'translateY(0)': 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+            {this.props.children}
+        </div>
+      </>
+    );
+  }
 };
 
 export default Modal;
