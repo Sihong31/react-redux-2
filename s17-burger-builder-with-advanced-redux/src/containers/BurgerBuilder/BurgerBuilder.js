104c44ccbc95c6a4a60c7c9db70d17dafd0a1b16
Sihong Huang <sihongspring2011@gmail.com>
2019-01-09 18:10:57 -0500

    start work on advanced redux

---
+++ s17-burger-builder-with-advanced-redux/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../../store/actions/index';
+
+import axios from '../../axios-orders';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
+class BurgerBuilder extends Component {
+  state = {
+    purchasing: false,
+  }
+
+  componentDidMount() {
+    this.props.onInitIngredients();
+  }
+
+  updatePurchaseState(ingredients) {
+    // check purchaseable state depending on how many ingredients have been selected for burger
+    const sum = Object.keys(ingredients)
+      .map(ingredientKey => {
+        return ingredients[ingredientKey]
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    return sum > 0;
+  }
+
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  }
+
+  purchaseContinueHandler = () => {
+    this.props.history.push('/checkout');
+  }
+
+  render() {
+    const disabledInfo = {
+      ...this.props.ingredients
+    }
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+
+    let orderSummary = null;
+    let burger = this.props.error ? <p>Ingredients can't be loaded</p> : <Spinner />
+    if (this.props.ingredients) {
+      burger = (
+        <>
+          <Burger ingredients={ this.props.ingredients } />
+          <BuildControls 
+            ingredientAdded={this.props.onIngredientAdded} 
+            ingredientRemoved={this.props.onIngredientRemoved} 
+            disabled={disabledInfo}
+            purchaseable={this.updatePurchaseState(this.props.ingredients)}
+            price={this.props.totalPrice}
+            ordered={this.purchaseHandler} />
+        </>
+      );
+      orderSummary = (
+        <OrderSummary 
+          ingredients={this.props.ingredients}
+          price = {this.props.totalPrice} 
+          purchaseCancelled={this.purchaseCancelHandler} 
+          purchaseContinued={this.purchaseContinueHandler} />
+      );
+    }
+
+    return (
+      <>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
+          {orderSummary}
+        </Modal>
+        {burger}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice,
+    error: state.error
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIngredientAdded: (ingName) => dispatch(actionCreators.addIngredient(ingName)),
+    onIngredientRemoved: (ingName) => dispatch(actionCreators.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(actionCreators.initIngredients())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
---
+++ s17-burger-builder-with-advanced-redux/src/hoc/Layout/Layout.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/Sidedrawer/Sidedrawer';
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => { 
+      return { showSideDrawer: !prevState.showSideDrawer }
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Toolbar toggle={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} toggle={this.sideDrawerToggleHandler} />
+        <main className= {classes.Content}>
+          { this.props.children }
+        </main>
+      </>
+    );
+  }
+}
+
+export default Layout;
