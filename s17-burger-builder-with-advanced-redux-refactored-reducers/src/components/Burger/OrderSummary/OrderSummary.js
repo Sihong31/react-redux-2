3b048907812a917f267b9c2f42241ede55739fe0
Sihong Huang <sihongspring2011@gmail.com>
2019-01-10 12:32:51 -0500

    refactoring reducers for burger builder project

---
+++ s17-burger-builder-with-advanced-redux-refactored-reducers/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+
+  componentWillUpdate() {
+    console.log('order summary will update');
+  }
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+    .map(ingredientKey => {
+      return (
+        <li key={ingredientKey}>
+          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
+        </li>
+      )
+    })
+
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>Delicious Burger with the following ingredients:</p>
+        <ul>
+          { ingredientSummary }
+        </ul>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </>
+    );
+  }
+}
+
+export default OrderSummary;
